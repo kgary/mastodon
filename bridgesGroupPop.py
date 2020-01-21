64d16032f7ce25fb3f7523c2b871b895984227b3
Jacob Wallert <jawaller@asu.edu>
2020-01-20 22:13:34 -0700

    added already following check, and admin/mod follows

--- bridgesGroupPop.py
+++ bridgesGroupPop.py
@@ -53,6 +53,8 @@ def group_follows(con):
     tokenExec = "SELECT token FROM oauth_access_tokens WHERE resource_owner_id = %s;"
     con.execute(tokenExec, (id, ))
     oauth = con.fetchone()[0]
+    if len(oauth) < 2:
+        sys.exit('No auth token for user, cannot login/modify')
 
     gListExec = "SELECT account_id FROM users WHERE heal_group_name = %s;"
     con.execute(gListExec, (group, ))
@@ -61,18 +63,25 @@ def group_follows(con):
     fListExec = "SELECT target_account_id FROM follows WHERE account_id = %s;"
     con.execute(fListExec, (id, ))
     followList = con.fetchall()
-    follow_user(groupList, oauth)
 
-def follow_user(u, oauth):
+    modExec = "SELECT account_id FROM users where admin = 't' or moderator = 't';"
+    con.execute(modExec)
+    modList = con.fetchall()
+    groupList += modList
+    follow_user(groupList, followList, oauth)
+
+def follow_user(u, f, oauth):
     form = {'authenticity_token': payload['auth_token']}
     headers = {'Authorization': 'Bearer ' + oauth}
     endpoint = '/api/v1/accounts/{id}/follow'
     for user in u:
-        if user[0] != id:
+        if user[0] != id and user[0] not in f:
             r = requests.post('http://localhost:3000' + endpoint.replace('{id}', str(user[0])), headers=headers, data=form)
             print("Followed: " + str(user[0]))
 
 with connection.cursor() as con:
+    #if isnewuser:
     mod_user(con)
-
+    #else:
+        #newfollows
 con.close()
