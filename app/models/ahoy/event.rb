31138187e7b940554a182f361dbdb6ce001f5921
Mason Cole <mcole18@asu.edu>
2020-08-04 09:19:26 -0700

    add flat object for csv export on events nested properties

--- app/models/ahoy/event.rb
+++ app/models/ahoy/event.rb
@@ -17,4 +17,22 @@ class Ahoy::Event < ApplicationRecord
 
   belongs_to :visit
   belongs_to :user, optional: true
+
+  def self.to_csv
+    events = all.as_json
+    events.each do |event|
+      event.merge!(event["properties"])
+      event.delete("properties")
+    end
+    csv = ::CSV.generate do |csv|
+      csv << events.first.keys
+      events.each do |hash|
+        csv << hash.values
+      end
+    end
+  end
 end
+
+#File.open('eventtest.csv', 'w') do |f|
+#  f.write(csv)
+#end
